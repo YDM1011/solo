7fc61b7d1f269f72316fe427f58bb03d82270fb3
YDM1011 <ydm101194@gmail.com>
2018-12-12 21:17:23 +0200

    test to dev

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.use((req,res,next)=>{
 app.use('/', api);
 app.get("/", function(req, res, next) {
     if (req.cookies['sid']) {
-        next
+        next()
     }else{
         res.render('index4', {title: 'Express'});
     }
