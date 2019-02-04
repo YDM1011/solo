e81c7d4eb3476a4160c13d80e22c3e214c56e2cd
MihaiLNikolaevich <mihaill.nik0laevich@gmail.com>
2019-02-04 17:44:44 +0200

    fixed

--- solo-app/src/app/navbar/mobile-menu.directive.ts
+++ solo-app/src/app/navbar/mobile-menu.directive.ts
@@ -1,16 +1,14 @@
-import {Directive, ElementRef, HostListener, Input} from '@angular/core';
+import {Directive, ElementRef, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appMobileMenu]'
 })
 export class MobileMenuDirective {
-  @Input() menuClose: any;
 
   constructor (private el:ElementRef) {}
-  private status: boolean = ( window.innerWidth < 992 &&  window.innerHeight < 550) || ( window.innerWidth < 500 &&  window.innerHeight < 992);
 
   ngOnInit() {
-    if (this.status){
+    if (( window.innerWidth < 992 &&  window.innerHeight < 550) || ( window.innerWidth < 500 &&  window.innerHeight < 992)){
       window.addEventListener('resize', this.resize, false);
       document.body.addEventListener('touchstart', this.touchS, false);
       document.body.addEventListener('touchend', this.touchE, false);
@@ -28,19 +26,15 @@ export class MobileMenuDirective {
     $event.stopPropagation();
     this.end = $event.changedTouches[0].pageX;
     if (window.innerWidth - 25 <= this.start && this.start - this.end >= 25) {
-      this.el.nativeElement.style.right = '0';
-      this.el.nativeElement.val = false;
+      this.el.nativeElement.open = true;
     }
   };
   resize = (): void => {
-    this.el.nativeElement.val = false;
-    this.el.nativeElement.style.right = '-100%';
+    this.el.nativeElement.open = false;
     if (window.innerWidth > 992) {
       this.el.nativeElement.style.transition = 'right 0s';
     } else {
-      setTimeout(  () => {
-        this.el.nativeElement.style.transition = 'right .2s ease-in-out';
-      }, 0)
+      setTimeout(  () => this.el.nativeElement.style.transition = 'right .2s ease-in-out', 0)
     }
   };
 
@@ -54,21 +48,7 @@ export class MobileMenuDirective {
     if(event.touches){
       event.stopPropagation();
       this.end = event.changedTouches[0].pageX;
-      if (this.end - this.start > 50) this.el.nativeElement.style.right = '-100%';
-      this.el.nativeElement.val = false;
-    }
-  }
-  @HostListener('click', ['$event']) onClick(event): void {
-    this.close(event);
-  }
-  close(ev) {
-    let tg = ev.target;
-    if (this.el.nativeElement !== tg){
-      while (tg != this.menuClose) {
-        tg = tg.parentElement;
-      }
-      if (tg == this.menuClose) this.el.nativeElement.style.right = '-100%';
-      this.el.nativeElement.val = false;
+      if (this.end - this.start > 50) this.el.nativeElement.open = false;
     }
   }
 }
