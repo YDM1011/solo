62ddedf881f9bf83589e0d502a675601b2c037da
hammer <>
2019-02-10 15:52:25 +0200

    not conf

--- establishments/src/app/commponents/header/header.component.ts
+++ establishments/src/app/commponents/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import {environment} from '../../../environments/environment';
+import {ApiService} from "../../service/api.service";
 
 @Component({
   selector: 'app-header',
@@ -14,9 +15,13 @@ export class HeaderComponent implements OnInit {
   public access = false;
   public host: string = environment.apiDomain;
   public arrayEts: any = [];
+  public friends: any = [];
+  public arrEts: any = [];
   public searchText = '';
   public popPreProd: boolean = false;
-  constructor(private cookie: CookieService) { }
+  constructor(
+    private cookie: CookieService,
+    private api: ApiService) { }
 
   ngOnInit() {
     this.id = this.cookie.get('userid');
@@ -29,5 +34,16 @@ export class HeaderComponent implements OnInit {
     const s = this;
     s.user = (data);
     s.access = true;
+  }
+  goSearch(e){
+    let s = this;
+    console.log(e);
+    if(e){
+      this.api.get('search?search="'+e+'"')
+        .then((res: any) => {
+          s.friends = res.users;
+          s.arrEts = res.est;
+        });
+    }
   }
 }
