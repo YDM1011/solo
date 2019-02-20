da69bb2212f93d6613f778cf22a897d10ab696dc
MihaiLNikolaevich <MihaiLL.Nik0laevich@gmail.com>
2019-02-21 01:30:34 +0200

    fixed

--- establishments/src/app/init-layout/left-bar-scroll.directive.ts
+++ establishments/src/app/init-layout/left-bar-scroll.directive.ts
@@ -4,15 +4,15 @@ import {Directive, ElementRef, Input} from '@angular/core';
   selector: '[appLeftBarScroll]'
 })
 export class LeftBarScrollDirective {
-
+  public bar: HTMLElement;
   constructor (private el:ElementRef) {}
-  @Input ('appLeftBarScroll') bar: any;
 
   ngOnInit() {
     if ( window.innerWidth >= 768 && window.innerHeight > 500) window.addEventListener('scroll', this.scroll, false);
     if ( window.innerWidth >= 768 && window.innerHeight > 500) window.addEventListener('resize', this.resize, false);
     this.hrhHeight = document.querySelector('nav').clientHeight +5;
-
+    this.bar = document.querySelector('.cont_left-bar');
+    window.addEventListener('orientationchange', this.orientation, true);
   }
   ngOnDestroy() {
     if ( window.innerWidth >= 768 && window.innerHeight > 500) window.removeEventListener('scroll', this.scroll, false);
@@ -24,9 +24,11 @@ export class LeftBarScrollDirective {
   resize = (): void => {
     this.hrhHeight = document.querySelector('nav').clientHeight + 5;
   };
-
+  orientation = (): void => {
+    this.el.nativeElement.style.width = this.bar.clientWidth + 'px';
+  };
   scroll = (): void => {
-    this.pageY = this.bar.y + window.pageYOffset - this.hrhHeight;
+    this.pageY = this.bar.getBoundingClientRect().top + window.pageYOffset - this.hrhHeight;
     (this.pageY <= window.pageYOffset && window.pageYOffset > 0) ? this.fixed() : this.static();
   };
 
--- solo-app/src/app/dashboard/left-bar-scroll.directive.ts
+++ solo-app/src/app/dashboard/left-bar-scroll.directive.ts
@@ -1,17 +1,19 @@
-import {Directive, ElementRef, Input} from '@angular/core';
+import {Directive, ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appLeftBarScroll]'
 })
 export class LeftBarScrollDirective {
+  public bar: HTMLElement;
 
   constructor (private el:ElementRef) {}
-  @Input ('appLeftBarScroll') bar: any;
 
   ngOnInit() {
     if ( window.innerWidth >= 768 && window.innerHeight > 500) window.addEventListener('scroll', this.scroll, false);
     if ( window.innerWidth >= 768 && window.innerHeight > 500) window.addEventListener('resize', this.resize, false);
     this.hrhHeight = document.querySelector('nav').clientHeight +5;
+    this.bar = document.querySelector('.cont_left-bar');
+    window.addEventListener('orientationchange', this.orientation, true);
 
   }
   ngOnDestroy() {
@@ -25,8 +27,11 @@ export class LeftBarScrollDirective {
     this.hrhHeight = document.querySelector('nav').clientHeight + 5;
   };
 
+  orientation = (): void => {
+    this.el.nativeElement.style.width = this.bar.clientWidth + 'px';
+  };
   scroll = (): void => {
-    this.pageY = this.bar.y + window.pageYOffset - this.hrhHeight;
+    this.pageY = this.bar.getBoundingClientRect().top + window.pageYOffset - this.hrhHeight;
     (this.pageY <= window.pageYOffset && window.pageYOffset > 0) ? this.fixed() : this.static();
   };
 
